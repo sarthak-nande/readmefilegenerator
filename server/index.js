d7831aaccd6167df72da2464af40e7a0ba12f3b0
sarthu210 <sarthu102@gmail.com>
2025-07-04 18:01:09 +0530

    Configuration Updated

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ import generateRoute from "./routes/userRoutes.js"
 const app = express();
 app.use(express.json());
 
+app.get("/" , (req,res) => {
+  return res.status(200).send({"message" : "Server is working!"})
+})
+
 app.use("/api" , generateRoute);
 
 app.listen(3000, () => {
