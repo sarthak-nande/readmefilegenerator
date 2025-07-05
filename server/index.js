ee205b548b23f44a6596f80c6c868e466492b049
sarthu210 <sarthu102@gmail.com>
2025-07-05 11:27:14 +0530

    New Cors Added

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://readmefilegenerator.vercel.app/', 
   credentials: true               
 }));
 
